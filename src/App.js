f2f0401e54f6d676b1da2ad156c4210c2c008d81
thadathilsinan <thadathilsinan@gmail.com>
2021-01-14 15:31:16 +0530

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import Button from "react-bootstrap/Button";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Button variant="outline-primary">Primary</Button>
+      Hello World
+    </div>
+  );
 }
 
 export default App;
