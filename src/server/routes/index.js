ecbcb0c5fdc531d4fe716ffd02a09bf71b7e38e4
thadathilsinan <thadathilsinan@gmail.com>
2021-01-17 16:07:22 +0530

    Setup backend for Yuvashiksha Added mongoose and connected to the MongoDB server Added Schema for Login

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+  .get("/", function (req, res, next) {
+    res.json({
+      batches: ["CS", "BSC"],
+    });
+    next();
+  })
+  .get("/1", (req, res, next) => {
+    let error = new Error("Access to /admin/1 is not allowed");
+    error.status = 500;
+    next(error);
+  });
 
 module.exports = router;
