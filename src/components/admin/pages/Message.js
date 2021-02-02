cc3d0294f7a568f8170d65fbbf481b3f4332a350
gladsonsaji <gladsonsaji4@gmail.com>
2021-02-02 18:43:46 +0530

    changed message jumburton to listitem

--- src/components/admin/pages/Message.js
+++ src/components/admin/pages/Message.js
@@ -4,7 +4,7 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import { Link, Route, withRouter } from "react-router-dom";
 import Messagecontent from "./Messagecontent";
 import Messagelist from "./Messagelist";
-
+import ListItem from "../../ui-elements/ListItem/ListItem";
 import { connect } from "react-redux";
 
 let mapDispatchToProps = (dispatch) => {
@@ -25,15 +25,17 @@ class Message extends React.Component {
   render() {
     return (
       <div className="container  col-md-10 mt-1 " onClick={this.storeMessage}>
-        <Jumbotron onCli>
-          <Container className="mt-1" style={{ height: "40px" }}>
-            <div className="mt-1">
-              <p class="text-left"> FROM : {this.props.message.from}</p>
-              <p class="text-left">DATE : {this.props.message.date}</p>
-              <p class="text-left">TIME : {this.props.message.time}</p>
-            </div>
-          </Container>
-        </Jumbotron>
+        <ListItem height="120px">
+          {{
+            left: (
+              <span>
+                <p class="text-left"> FROM : {this.props.message.from}</p>
+                <p class="text-left">DATE : {this.props.message.date}</p>
+                <p class="text-left">TIME : {this.props.message.time}</p>
+              </span>
+            ),
+          }}
+        </ListItem>
       </div>
     );
   }
