0dce5b245493af033c8e979e4561a5c2250088bc
gladsonsaji <gladsonsaji4@gmail.com>
2021-02-05 12:18:12 +0530

    Updated gitignore with /.eslintcache Added disable and delete features in usermanagement in admin module

--- src/components/admin/pages/UserManagment/Teachers.js
+++ src/components/admin/pages/UserManagment/Teachers.js
@@ -1,31 +1,74 @@
 import React, { Component } from "react";
 import ListItem from "../../../ui-elements/ListItem/ListItem";
 import { Row, Col, Button } from "react-bootstrap";
+import http from "../../../../shared/http";
 export default class Teachers extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: [
-        {
-          name: "Teacher name",
-          idNumber: "12345",
-          department: "CS",
-          email: "hkjhksd@njdsf.fsd",
-        },
-      ],
+      userData: [],
     };
   }
 
+  deleteClickListener = (item) => {
+    let confirmation = window.confirm("Are you sure to want to delete?");
+
+    if (confirmation) {
+      http(
+        "POST",
+        "/admin/usermanagement/teacher/delete",
+        { teacher: item },
+        (res) => {
+          if (res.status == 200) {
+            alert("Teacher account deleted successfully");
+
+            document.getElementById(item._id).style.display = "none";
+          } else {
+            alert("Error deleting teacher account");
+          }
+        }
+      );
+    }
+  };
+
   disableClickListener = (event, item) => {
     if (event.target.value == "Disable") {
-      event.target.setAttribute("value", "Enable");
-      event.target.classList.remove("btn-warning");
-      event.target.classList.add("btn-success");
+      let confirmation = window.confirm("Are sure to Disable account?");
+
+      if (confirmation) {
+        http(
+          "POST",
+          "/admin/usermanagement/teacher/disable",
+          { teacher: item },
+          (res) => {
+            if (res.status == 200) {
+              alert("Teacher Account disabled Successfully");
+              this.changeToEnable(event);
+            } else {
+              alert("Error disabling teacher account");
+            }
+          }
+        );
+      }
     } else {
-      event.target.setAttribute("value", "Disable");
-      event.target.classList.remove("btn-success");
-      event.target.classList.add("btn-warning");
+      let confirmation = window.confirm("Are you sure to Enable account?");
+
+      if (confirmation) {
+        this.changeToDisable(event);
+      }
     }
+  };
+
+  changeToDisable = (event) => {
+    event.target.setAttribute("value", "Disable");
+    event.target.classList.remove("btn-success");
+    event.target.classList.add("btn-warning");
+  };
+
+  changeToEnable = (event) => {
+    event.target.setAttribute("value", "Enable");
+    event.target.classList.remove("btn-warning");
+    event.target.classList.add("btn-success");
   };
 
   getTeacherList = () => {
@@ -46,7 +89,11 @@ export default class Teachers extends Component {
                     <p class="text-left">{item.department}</p>
                   </span>{" "}
                   <Col className="text-left">
-                    <Button type="submit" className=" mr-3 btn btn-danger">
+                    <Button
+                      type="submit"
+                      className=" mr-3 btn btn-danger"
+                      onClick={() => this.deleteClickListener(item)}
+                    >
                       Delete
                     </Button>
                   </Col>
@@ -74,6 +121,20 @@ export default class Teachers extends Component {
 
     return teachers;
   };
+
+  getUserData = () => {
+    http("GET", "/admin/usermanagement/teacher", null, (res) => {
+      if (res.status == 200) {
+        this.setState({ userData: res.data.data });
+      } else {
+        alert("Error during fetching teacher data from server");
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.getUserData();
+  }
 
   render() {
     return <>{this.getTeacherList()}</>;
--- src/server/routes/admin.js
+++ src/server/routes/admin.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 let Signin = require("../schema/signin");
 let Teachers = require("../schema/teachers");
+let Students = require("../schema/students");
 
 var router = express.Router();
 
@@ -46,4 +47,41 @@ router.post("/verifyaccount/reject", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+router.get("/usermanagement/teacher", (req, res, next) => {
+  Teachers.find({})
+    .then((response) => {
+      res.statusCode = 200;
+      res.json({ data: response });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post("/usermanagement/teacher/delete", (req, res, next) => {
+  Teachers.findOneAndDelete({ _id: req.body.teacher._id })
+    .then((response) => {
+      res.statusCode = 200;
+      res.end("Successfully deleted teacher");
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/usermanagement/teacher/disable", (req, res, next) => {
+  Teachers.findOne({ _id: req.body.teacher._id })
+    .then((response) => {
+      if (response) {
+        Signin.findOneAndUpdate(
+          { username: req.body.teacher.username },
+          { accountStatus: "disabled" }
+        )
+          .then((resp) => {
+            res.statusCode = 200;
+            res.end("Teacher account disabled successfully");
+          })
+          .catch((err) => next(err));
+      }
+    })
+    .catch((err) => next(err));
+});
 module.exports = router;
