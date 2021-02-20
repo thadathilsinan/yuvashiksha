66e23395047dcdd44698fe8fddc7d5e89cdf0474
thadathilsinan <thadathilsinan@gmail.com>
2021-02-20 14:07:23 +0530

    Redesigned the register.js route to work with the new database design

--- src/server/routes/register.js
+++ src/server/routes/register.js
@@ -12,6 +12,7 @@ let sendMail = require("../functions/sendMail");
 
 //Importing required Mongoose Models
 let Users = require("../schema/Users");
+let Otp = require("../schema/Otp");
 
 var router = express.Router();
 
@@ -42,235 +43,178 @@ Your OTP for the account signup is : ${otp}`;
   sendMail(destination, mailSubject, mailBody);
 };
 
-router
-  .post("/", (req, res, next) => {
-    let otp = generateOtp();
+router.post("/", (req, res, next) => {
+  /**
+   * ROUTE FOR:
+   * When user click 'Verify Email' Button in client side (FIRST STEP IN USER REGISTRATION)
+   */
 
-    Users.findOne({ registerNumber: req.body.registerNumber });
-  })
-  .post("/resendotp", (req, res, next) => {
-    let otp = generateOtp();
+  Users.findOne({ registerNumber: req.body.registerNumber })
+    .then((user) => {
+      if (user) {
+        //User with same register number already exists
+        res.statusCode = 203;
+        res.end("User with the register number already exist");
+      }
 
-    if (req.body.cookies.accountType == "student") {
-      StudentSignup.findOneAndUpdate(
-        {
-          admissionNumber: req.body.cookies.admissionNumber,
-          email: req.body.cookies.email,
-        },
-        { otp: otp }
-      )
-        .then((user) => {
-          console.log("New OTP: ", user.otp);
-          sendOtpMail(user.otp, user.email);
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({
-            admissionNumber: user.admissionNumber,
-            email: user.email,
-            accountType: req.body.cookies.accountType,
+      //Creating a new user
+      let newUser = new Users({
+        accountType: req.body.accountType,
+        accountStatus: "signup-incomplete",
+        name: req.body.name,
+        email: req.body.email,
+        registerNumber: req.body.registerNumber,
+      });
+
+      //Checking user type and add neccessary data to the new User object
+      if (req.body.accountType == "student") {
+        newUser.class = req.body.class;
+        newUser.parentEmail = req.body.parentEmail;
+      } else if (req.body.accountType == "teacher") {
+        newUser.department = req.body.department;
+      }
+
+      //Saving the new user into the database
+      newUser
+        .save()
+        .then(async (user) => {
+          //Generating OTP for the user
+          let otp = generateOtp();
+
+          //Creating an expiry date for the new OTp
+          let expiryDate = new Date();
+          expiryDate.setMinutes(expiryDate.getMinutes() + 15);
+
+          //Creating a new document in the OTP collection
+          let newOtp = new Otp({
+            userId: user._id,
+            otp: otp,
+            expiry: expiryDate,
           });
-        })
-        .catch((err) => next(err));
-    } else if (req.body.cookies.accountType == "teacher") {
-      TeacherSignup.findOneAndUpdate(
-        {
-          idNumber: req.body.cookies.idNumber,
-          email: req.body.cookies.email,
-        },
-        { otp: otp }
-      )
-        .then((user) => {
-          console.log("New OTP: ", user.otp);
-          sendOtpMail(user.otp, user.email);
+
+          //Saving newOtp into the OTP collection
+          await newOtp.save().catch((err) => next(err));
+
+          //All steps in the STEP 1 of REGISTRATION IS COMPLETED SUCCESSFULLY
+          console.log(
+            "New User account added to database",
+            "REGISTER ACCOUNT STEP 1 COMPLETED"
+          );
+
+          //Sending Email with otp to the user
+          sendOtpMail(otp, user.email);
+
           res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({
-            idNumber: user.idNumber,
-            email: user.email,
-            accountType: req.body.cookies.accountType,
-          });
+          res.end("Registration part 1 completed");
         })
         .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+});
+
+/**
+ * ROUTE TO RESEND OTP FOR THE USER
+ */
+router.post("/resendotp", (req, res, next) => {
+  Otp.findOne({ userId: req.body.userId })
+    .then(async (otp) => {
+      if (otp) {
+        //Generating OTP for the user
+        let newOtp = generateOtp();
+
+        //Creating an expiry date for the new OTp
+        let expiryDate = new Date();
+        expiryDate.setMinutes(expiryDate.getMinutes() + 15);
+
+        //Setting new otp and expiry date
+        otp.otp = newOtp;
+        otp.expiry = expiryDate;
+
+        //Saving the changes to the db
+        await otp.save().catch((err) => next(err));
+
+        //Sending email to the user with new otp
+        await Users.findOne({ _id: req.body.userId })
+          .then((user) => {
+            sendOtpMail(newOtp, user.email);
+          })
+          .catch((err) => next(err));
+
+        res.statusCode = 200;
+        res.end("OTP resend successfully");
+      } else {
+        res.statusCode = 203;
+        res.end("User account does not exists, Cannot resend OTP");
+      }
+    })
+    .catch((err) => next(err));
+});
+
+/**
+ * ROUTE FOR VERIFYING OTP FOR VERIFIYING USER EMAIL
+ * PART 2 OF REGISTRATION OF NEW ACCOUNT
+ */
+router.post("/otp", (req, res, next) => {
+  Otp.findOne({ userId: req.body.userId }).then((otp) => {
+    if (otp) {
+      //OTP Verification
+      if (otp.otp == req.body.otp) {
+        res.statusCode = 200;
+        res.end("OTP Verified successfully");
+      } else {
+        res.statusCode = 203;
+        res.end("OTP is incorrect");
+      }
     } else {
-      console.log(req);
-      res.statusCode = 404;
-      res.end("Cookie not found");
-    }
-  })
-  .post("/otp", (req, res, next) => {
-    if (req.body.cookies.accountType == "student") {
-      StudentSignup.findOne({
-        admissionNumber: req.body.cookies.admissionNumber,
-        email: req.body.cookies.email,
-      })
-        .then((user) => {
-          if (!user) {
-            res.statusCode = 203;
-            res.end(
-              "No user found.\nCookies may be cleared from your browser.\nYou are required to restart the signup process."
-            );
-          }
-
-          if (user.otp == req.body.otp) {
-            console.log("OTP successfully verified");
-            res.statusCode = 200;
-            res.end("OTP successfully verified");
-          } else {
-            console.log("OTP is incorrect");
-            res.statusCode = 203;
-            res.end("OTP incorrect");
-          }
-        })
-        .catch((err) => next(err));
-    } else if (req.body.cookies.accountType == "teacher") {
-      TeacherSignup.findOne({
-        idNumber: req.body.cookies.idNumber,
-        email: req.body.cookies.email,
-      })
-        .then((user) => {
-          if (!user) {
-            res.statusCode = 203;
-            res.end(
-              "No user found.\nCookies may be cleared from your browser.\nYou are required to restart the signup process."
-            );
-          }
-
-          if (user.otp == req.body.otp) {
-            console.log("OTP successfully verified");
-            res.statusCode = 200;
-            res.end("OTP successfully verified");
-          } else {
-            console.log("OTP is incorrect");
-            res.statusCode = 203;
-            res.end("OTP incorrect");
-          }
-        })
-        .catch((err) => next(err));
-    } else {
-      console.log(req);
-      res.statusCode = 404;
-      res.end("Cookie not found");
-    }
-  })
-  .post("/finish", (req, res, next) => {
-    if (req.body.cookies.accountType == "student") {
-      StudentSignup.findOneAndUpdate(
-        {
-          admissionNumber: req.body.cookies.admissionNumber,
-          email: req.body.cookies.email,
-        },
-        {
-          password: req.body.password,
-        }
-      )
-        .then((user) => {
-          if (!user) {
-            res.statusCode = 203;
-            res.end(
-              "No user found.\nCookies may be cleared from your browser.\nYou are required to restart the signup process."
-            );
-          } else {
-            Students.create({
-              username: user.email,
-              name: user.name,
-              admissionNumber: user.admissionNumber,
-              class: user.class,
-              batch: user.batch,
-              email: user.email,
-              parentEmail: user.parentEmail,
-            })
-              .then((newUser) => {
-                Signin.create({
-                  username: newUser.username,
-                  password: req.body.password,
-                  accountType: "student",
-                  accountStatus: "not-activated",
-                })
-                  .then((newStudent) => {
-                    console.log(
-                      "Added new Student account into Students and Signin DB"
-                    );
-
-                    res.statusCode = 200;
-                    res.end("Signup Successful");
-                  })
-                  .catch((err) => {
-                    console.log("Error IN SIGIN");
-                    next(err);
-                  });
-              })
-              .catch((err) => {
-                console.log("ERROR IN STUDENTS");
-                next(err);
-              });
-          }
-        })
-        .catch((err) => {
-          console.log("ERROR IN STUDENT SIGNUP");
-          next(err);
-        });
-    } else if (req.body.cookies.accountType == "teacher") {
-      TeacherSignup.findOneAndUpdate(
-        {
-          idNumber: req.body.cookies.idNumber,
-          email: req.body.cookies.email,
-        },
-        { password: req.body.password }
-      )
-        .then((user) => {
-          if (!user) {
-            res.statusCode = 203;
-            res.end(
-              "No user found.\nCookies may be cleared from your browser.\nYou are required to restart the signup process."
-            );
-          } else {
-            Teachers.create({
-              username: user.email,
-              mentor: null,
-              hod: null,
-              name: user.name,
-              idNumber: user.idNumber,
-              email: user.email,
-              department: user.department,
-            })
-              .then((newTeacher) => {
-                Signin.create({
-                  username: newTeacher.username,
-                  password: req.body.password,
-                  accountType: "teacher",
-                  accountStatus: "not-activated",
-                })
-                  .then((newTeacher) => {
-                    console.log(
-                      "Added new Teacher account into Teachers   and Signin DB"
-                    );
-
-                    res.statusCode = 200;
-                    res.end("Signup Successful");
-                  })
-                  .catch((err) => next(err));
-              })
-              .catch((err) => next(err));
-          }
-        })
-        .catch((err) => next(err));
-    } else {
-      console.log(req);
-      res.statusCode = 404;
-      res.end("Cookie not found");
+      res.statusCode = 203;
+      res.end("User account not found");
     }
   });
+});
 
+/**
+ * FINAL PART OF THE USER ACCOUNT REGISTERATION
+ * setting the password the user account
+ */
+router.post("/finish", (req, res, next) => {
+  Users.findOne({ _id: req.body.userId })
+    .then(async (user) => {
+      if (user) {
+        //Setting the password for the user and changing accountStatus
+        user.password = req.body.password;
+        user.accountStatus = "not-activated";
+
+        //Saving changes to the db
+        await user.save().catch((err) => nativeTouchData(err));
+
+        res.statusCode = 200;
+        res.end("User registration completed");
+      } else {
+        res.statusCode = 203;
+        res.end("User account not found");
+      }
+    })
+    .catch((err) => next(err));
+});
+
+/**
+ * ROUTE FOR GOOGLE SIGNIN/SIGNINUP
+ * THIS WILL BE REDIRECTED TO GOOGLE'S SERVER PAGE TO ENTER GOOGLE USER CREDENTIALS
+ */
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
+/**
+ * This route is automatically called after the user successfully login to google
+ */
 router.get(
   "/redirect",
   passport.authenticate("google", { failureRedirect: "/" }),
   function (req, res) {
-    Signin.findOne({ googleId: req.user._json.sub })
+    //Checking if user account already exists with this google user
+    Users.findOne({ googleId: req.user._json.sub })
       .then((user) => {
         if (user) {
           if (user.accountType == "student")
@@ -278,6 +222,7 @@ router.get(
           else if (user.accountType == "teacher")
             res.redirect("http://localhost:3000/teacher");
         } else {
+          //Continue to google user signup page
           res.redirect(
             `http://localhost:3000/signup/google?name=${req.user._json.name}&id=${req.user._json.sub}&email=${req.user._json.email}`
           );
@@ -287,141 +232,41 @@ router.get(
   }
 );
 
-router.post("/googlesignup", (req, res, next) => {
+/**
+ * GOOGLE VERIFED ACCOUNT SIGNUP PROCEDURE
+ */
+router.post("/googlesignup", async (req, res, next) => {
+  await Users.findOne({ registerNumber: req.body.registerNumber }).then(
+    (user) => {
+      if (user) {
+        res.statusCode = 203;
+        res.end("User already exist with same register number");
+      }
+    }
+  );
+
+  let newUser = new Users({
+    accountType: req.body.accountType,
+    accountStatus: "not-activated",
+    name: req.body.name,
+    email: req.body.email,
+    registerNumber: req.body.registerNumber,
+    googleId: req.body.googleId,
+  });
+
+  //Checking user type and add neccessary data to the new User object
   if (req.body.accountType == "student") {
-    StudentSignup.findOne({ admissionNumber: req.body.admissionNumber })
-      .then((user) => {
-        if (user) {
-          let error = new Error("User already Exist.");
-          next(error);
-          res.end();
-        } else {
-          StudentSignup.create({
-            name: req.body.name,
-            admissionNumber: req.body.admissionNumber,
-            class: req.body.class,
-            batch: req.body.batch,
-            email: req.body.email,
-            parentEmail: req.body.parentEmail,
-            otp: null,
-            password: null,
-            googleSignup: true,
-            googleId: req.body.googleId,
-          })
-            .then((user) => {
-              console.log(
-                "New user data added to StudentSignup collection : " + user
-              );
-
-              Students.create({
-                username: user.email,
-                name: user.name,
-                admissionNumber: user.admissionNumber,
-                class: user.class,
-                batch: user.batch,
-                email: user.email,
-                parentEmail: user.parentEmail,
-              })
-                .then((newUser) => {
-                  Signin.create({
-                    username: req.body.email,
-                    password: null,
-                    accountType: "student",
-                    accountStatus: "not-activated",
-                    googleSignup: true,
-                    googleId: req.body.googleId,
-                  })
-                    .then((newStudent) => {
-                      console.log(
-                        "Added new Student account into Students and Signin DB"
-                      );
-
-                      res.statusCode = 200;
-                      res.end("Google Signup Success");
-                    })
-                    .catch((err) => {
-                      console.log("Error in Signin", err);
-                      next(err);
-                    });
-                })
-                .catch((err) => {
-                  console.log("Error in Students");
-                  next(err);
-                });
-            })
-            .catch((err) => {
-              console.log("Error in Student Signup");
-              next(err);
-            });
-        }
-      })
-      .catch((err) => {
-        next(err);
-      });
+    newUser.class = req.body.class;
+    newUser.parentEmail = req.body.parentEmail;
   } else if (req.body.accountType == "teacher") {
-    TeacherSignup.findOne({ idNumber: req.body.idNumber })
-      .then((user) => {
-        if (user) {
-          let error = new Error("User already Exist.");
-          next(error);
-        } else {
-          TeacherSignup.create({
-            name: req.body.name,
-            idNumber: req.body.idNumber,
-            email: req.body.email,
-            department: req.body.department,
-            otp: null,
-            password: null,
-            googleSignup: true,
-            googleId: req.body.googleId,
-          })
-            .then((user) => {
-              console.log(
-                "New user data added to TeacherSignup collection : " + user
-              );
-              Teachers.create({
-                username: user.email,
-                name: user.name,
-                idNumber: user.idNumber,
-                email: user.email,
-                department: user.department,
-              })
-                .then((newUser) => {
-                  Signin.create({
-                    username: newUser.username,
-                    password: null,
-                    accountType: "teacher",
-                    accountStatus: "not-activated",
-                    googleSignup: true,
-                    googleId: req.body.googleId,
-                  })
-                    .then((newStudent) => {
-                      console.log(
-                        "Added new Teacher account into Teachers and Signin DB"
-                      );
-
-                      res.statusCode = 200;
-                      res.end("Google Signup Success");
-                    })
-                    .catch((err) => {
-                      next(err);
-                    });
-                })
-                .catch((err) => {
-                  next(err);
-                });
-            })
-            .catch((err) => next(err));
-        }
-      })
-      .catch((err) => {
-        next(err);
-      });
-  } else {
-    console.log(req);
-    res.statusCode = 404;
-    res.end("Invalid Account type");
+    newUser.department = req.body.department;
   }
+
+  //Saving new user to the db
+  await newUser.save().catch((err) => next(err));
+
+  res.statusCode = 200;
+  res.end("User registration completed.");
 });
 
 module.exports = router;
