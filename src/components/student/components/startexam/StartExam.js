39337659d625a7183702ccd220dfd07d7652c122
gladsonsaji <gladsonsaji4@gmail.com>
2021-02-22 13:04:15 +0530

    added Start exam page and added disable function for coppy paste and right click

--- src/components/student/components/startexam/StartExam.js
+++ src/components/student/components/startexam/StartExam.js
@@ -3,8 +3,39 @@ import NavBar from "../../../ui-elements/navBar/NavBar";
 import ListItem from "../../../ui-elements/ListItem/ListItem";
 import { Button } from "react-bootstrap";
 import { AiOutlineCheck } from "react-icons/ai";
+import $ from "jquery";
 export class StartExam extends Component {
   render() {
+    $(document).ready(function () {
+      // Disables ctrl+v, ctrl+x, ctrl+c.
+      $("textarea").on("cut", function (e) {
+        $("#d2").text("cut. not allowed!");
+        e.preventDefault();
+      });
+      $("textarea").on("copy", function (e) {
+        $("#d2").text("copy. not allowed!");
+        e.preventDefault();
+      });
+      $("textarea").on("paste", function (e) {
+        $("#d2").text("paste. not allowed!");
+        e.preventDefault();
+      });
+
+      // Above all three can be combined into one, above is
+      // executed separately for understanding purposes.
+      /* $('textarea').on("cut copy paste", function(e) { 
+        $("#d2").text('right-click is disabled!'); 
+        e.preventDefault(); 
+        }); */
+
+      // Disables right-click.
+      $("textarea").mousedown(function (e) {
+        if (e.button == 2) {
+          e.preventDefault();
+          alert("right-click is disabled!");
+        }
+      });
+    });
     return (
       <div className="root">
         <NavBar>
@@ -27,7 +58,38 @@ export class StartExam extends Component {
           {{
             left: (
               <div id="leftListItem">
-                <p></p>
+                <p>DATE:DD-MM-YYYY</p>
+                <p>TIME:HH:MM:SS</p>
+                <p>Name of Examination:</p>
+              </div>
+            ),
+            right: (
+              <div id="rightListItem">
+                <p>Subject:</p>
+                <p>MARK:80</p>
+              </div>
+            ),
+          }}
+        </ListItem>
+        <ListItem height="150px">
+          {{
+            left: (
+              <div id="leftListItem">
+                <p>What is computer</p>
+                <p>
+                  <textarea
+                    cols="150"
+                    rows="5"
+                    type="textbox"
+                    id="ans"
+                    oncut="return false"
+                    oncopy="return false"
+                    onpaste="return false"
+                  ></textarea>{" "}
+                  <p id="d1"></p>
+                  <p id="d2"></p>
+                  <Button className="btn btn-secondary ml-3">canavs</Button>
+                </p>
               </div>
             ),
             right: (
