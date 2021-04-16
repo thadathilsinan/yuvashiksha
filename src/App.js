950cd6f7a16115d7201cc122d0c33a639ad960cb
thadathilsinan <thadathilsinan@gmail.com>
2021-04-16 10:55:33 +0530

    Changed Kiosk checking Now only students need to open app using laucher

--- src/App.js
+++ src/App.js
@@ -14,105 +14,8 @@ import $ from "jquery";
 import Canvas from "./components/ui-elements/Canvas/Canvas";
 import StudentMain from "./components/student/StudentMain";
 
-import { FaPowerOff } from "react-icons/fa";
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-      text: "INITIALIZING YUVASHIKSHA ...",
-      textHint: "",
-    };
-  }
-
-  //disable right click
-  preventRightClick = () => {
-    document.addEventListener("contextmenu", (event) => event.preventDefault());
-  };
-
-  //Prevent COPY ,PASTE
-  preventCopyPaste = () => {
-    $(document).ready(() => {
-      // Disables ctrl+v, ctrl+x, ctrl+c.
-      $(document).on("cut copy paste", function (e) {
-        alert("Cut, Copy, Paste are NOT allowed.");
-        e.preventDefault();
-      });
-    });
-  };
-
-  //Prevent keys for security in KIOSK mode
-  preventKeys = () => {
-    $(document).ready(() => {
-      $(document).keydown((event) => {
-        let key = event.which;
-
-        //Disabling keys
-        if (
-          key === 17 ||
-          key === 18 ||
-          key === 91 ||
-          key === 92 ||
-          key === 93 ||
-          key === 112 ||
-          key === 113 ||
-          key === 114 ||
-          key === 115 ||
-          key === 116 ||
-          key === 117 ||
-          key === 118 ||
-          key === 119 ||
-          key === 120 ||
-          key === 121 ||
-          key === 122 ||
-          key === 123 ||
-          key === 44 ||
-          event.ctrlKey ||
-          event.altKey
-        ) {
-          event.preventDefault();
-        }
-      });
-    });
-  };
-
-  //Check if running in kiosk mode or not
-  checkKiosk = () => {
-    if (
-      (window.screen.availHeight || window.screen.height - 30) <=
-      window.innerHeight
-    ) {
-      navigator.clipboard
-        .readText()
-        .then((text) => {
-          if (
-            text ===
-            "hjafjbsfmnzcbfisfhbjkadbnjkfgouidh;OHIDYR985r89wyrwqfpawis8uy89uy9f8fc5ra8eu9565qafusiug238q478"
-          ) {
-            this.setState({ show: true });
-          } else {
-            this.setState({ text: "OPEN YUVASHIKSHA USING THE LAUNCHER" });
-          }
-        })
-        .catch((err) => {
-          this.setState({
-            text: "CANNOT START YUVASHIKSHA, 'CLIPBOARD' PERMISSION BLOCKED",
-            textHint: "(RESTART LAUNCHER AND ALLOW PERMISSION)",
-          });
-        });
-    } else {
-      this.setState({ text: "OPEN YUVASHIKSHA USING THE LAUNCHER" });
-    }
-  };
-
   componentDidMount() {
-    //Disabling right click menu
-    this.preventRightClick();
-
-    //Prevent cut copy paste features
-    this.preventCopyPaste();
-
     //Replacing system default alert with custom alert
     window.alert = (text) => {
       $("body").append(
@@ -128,49 +31,22 @@ class App extends React.Component {
         </div>`
       );
     };
-
-    this.preventKeys();
-
-    this.checkKiosk();
   }
 
   render() {
     return (
       <BrowserRouter>
         <Provider store={store}>
-          {this.state.show ? (
-            <div>
-              {/* Routes of the app */}
-              <Route path="/" component={LoginRoute} />
-              <Route path="/student" component={StudentMain} />
-              <Route path="/teacher" component={TeacherMain} />
-              <Route path="/admin" component={AdminMain} />
+          <div>
+            {/* Routes of the app */}
+            <Route path="/" component={LoginRoute} />
+            <Route path="/student" component={StudentMain} />
+            <Route path="/teacher" component={TeacherMain} />
+            <Route path="/admin" component={AdminMain} />
 
-              {/* //Test route for development purposes */}
-              <Route path="/test"></Route>
-            </div>
-          ) : (
-            <div
-              className="d-flex align-items-center justify-content-center"
-              style={{ height: "100vh", flexDirection: "column" }}
-            >
-              {/* EXIT BUTTON */}
-              <div id="yuvashikshaExit">
-                <button
-                  className="btn btn-danger"
-                  title="Exit Yuvashiksha"
-                  onClick={() => {
-                    navigator.clipboard.writeText("close-yuvashiksha");
-                  }}
-                >
-                  <FaPowerOff />
-                </button>
-              </div>
-
-              <h2>{this.state.text}</h2>
-              {this.state.textHint ? <h5>{this.state.textHint}</h5> : null}
-            </div>
-          )}
+            {/* //Test route for development purposes */}
+            <Route path="/test"></Route>
+          </div>
         </Provider>
       </BrowserRouter>
     );
--- src/components/student/StudentMain.js
+++ src/components/student/StudentMain.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import http from "../../shared/http";
 import Student from "./Student";
+import { FaPowerOff } from "react-icons/fa";
+import $ from "jquery";
 
 class StudentMain extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { component: null };
+    this.state = {
+      component: null,
+      show: false,
+      text: "PLEASE WAIT ...",
+      textHint: "",
+    };
   }
 
   checkAuthentication = () => {
@@ -34,12 +41,131 @@ class StudentMain extends Component {
     );
   };
 
+  //Check if running in kiosk mode or not
+  checkKiosk = () => {
+    if (
+      (window.screen.availHeight || window.screen.height - 30) <=
+      window.innerHeight
+    ) {
+      navigator.clipboard
+        .readText()
+        .then((text) => {
+          if (
+            text ===
+            "hjafjbsfmnzcbfisfhbjkadbnjkfgouidh;OHIDYR985r89wyrwqfpawis8uy89uy9f8fc5ra8eu9565qafusiug238q478"
+          ) {
+            this.setState({ show: true });
+          } else {
+            this.setState({ text: "OPEN YUVASHIKSHA USING THE LAUNCHER" });
+          }
+        })
+        .catch((err) => {
+          this.setState({
+            text: "CANNOT START YUVASHIKSHA, 'CLIPBOARD' PERMISSION BLOCKED",
+            textHint: "(RESTART LAUNCHER AND ALLOW PERMISSION)",
+          });
+        });
+    } else {
+      this.setState({ text: "OPEN YUVASHIKSHA USING THE LAUNCHER" });
+    }
+  };
+
+  //disable right click
+  preventRightClick = () => {
+    document.addEventListener("contextmenu", (event) => event.preventDefault());
+  };
+
+  //Prevent COPY ,PASTE
+  preventCopyPaste = () => {
+    $(document).ready(() => {
+      // Disables ctrl+v, ctrl+x, ctrl+c.
+      $(document).on("cut copy paste", function (e) {
+        alert("Cut, Copy, Paste are NOT allowed.");
+        e.preventDefault();
+      });
+    });
+  };
+
+  //Prevent keys for security in KIOSK mode
+  preventKeys = () => {
+    $(document).ready(() => {
+      $(document).keydown((event) => {
+        let key = event.which;
+
+        //Disabling keys
+        if (
+          key === 17 ||
+          key === 18 ||
+          key === 91 ||
+          key === 92 ||
+          key === 93 ||
+          key === 112 ||
+          key === 113 ||
+          key === 114 ||
+          key === 115 ||
+          key === 116 ||
+          key === 117 ||
+          key === 118 ||
+          key === 119 ||
+          key === 120 ||
+          key === 121 ||
+          key === 122 ||
+          key === 123 ||
+          key === 44 ||
+          event.ctrlKey ||
+          event.altKey
+        ) {
+          event.preventDefault();
+        }
+      });
+    });
+  };
+
   componentDidMount() {
+    this.checkKiosk();
+
+    //Disabling right click menu
+    this.preventRightClick();
+
+    //Prevent cut copy paste features
+    this.preventCopyPaste();
+
+    this.preventKeys();
+
     this.checkAuthentication();
   }
 
   render() {
-    return <>{this.state.component ? this.state.component : null}</>;
+    return (
+      <>
+        {this.state.show ? (
+          this.state.component ? (
+            this.state.component
+          ) : null
+        ) : (
+          <div
+            className="d-flex align-items-center justify-content-center"
+            style={{ height: "100vh", flexDirection: "column" }}
+          >
+            {/* EXIT BUTTON */}
+            <div id="yuvashikshaExit">
+              <button
+                className="btn btn-danger"
+                title="Exit Yuvashiksha"
+                onClick={() => {
+                  navigator.clipboard.writeText("close-yuvashiksha");
+                }}
+              >
+                <FaPowerOff />
+              </button>
+            </div>
+
+            <h2>{this.state.text}</h2>
+            {this.state.textHint ? <h5>{this.state.textHint}</h5> : null}
+          </div>
+        )}
+      </>
+    );
   }
 }
 
