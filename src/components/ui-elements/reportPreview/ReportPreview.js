409f946e276b866f91a1c41a59e618100b4c94ab
thadathilsinan <thadathilsinan@gmail.com>
2021-05-28 12:56:45 +0530

    Completed the rendering of the Report in admin

--- src/components/ui-elements/reportPreview/ReportPreview.js
+++ src/components/ui-elements/reportPreview/ReportPreview.js
@@ -26,7 +26,7 @@ class ReportPreview extends Component {
         let item = (
           <tr key={i}>
             <th width="150px">{i}</th>
-            <td>{header[i]}</td>
+            <td>: {header[i]}</td>
           </tr>
         );
 
@@ -37,8 +37,95 @@ class ReportPreview extends Component {
     this.setState({ header: parsedHeader });
   };
 
+  //Parse the exams section of the page
+  parseExams = () => {
+    let exams = this.props.data.exams;
+    let parsedExams = [];
+    let count = 1;
+
+    for (let exam of exams) {
+      let studentDetails = [];
+      let sNo = 1;
+
+      for (let i in exam.studentData) {
+        let studentRow = (
+          <tr key={i}>
+            <td>{sNo}</td>
+            <td>{exam.studentData[i].name}</td>
+            <td>{i}</td>
+            <td>{exam.studentData[i].email}</td>
+            <td>
+              {exam.studentData[i].marks ? exam.studentData[i].marks : "---"}
+            </td>
+          </tr>
+        );
+
+        sNo++;
+        studentDetails.push(studentRow);
+      }
+
+      let item = (
+        <>
+          <h5>EXAM NO: {count}</h5>
+          <table className="table table-striped">
+            <thead>
+              <tr className="table-info">
+                <th colSpan="2">Exam Name</th>
+                <td colSpan="3">{exam.examData["EXAM NAME"]}</td>
+              </tr>
+              <tr className="table-info">
+                <th colSpan="2">Subject</th>
+                <td colSpan="3">{exam.examData.SUBJECT}</td>
+              </tr>
+              <tr className="table-info">
+                <th colSpan="2">Date</th>
+                <td colSpan="3">{exam.examData.DATE}</td>
+              </tr>
+              <tr className="table-info">
+                <th colSpan="2">Class</th>
+                <td colSpan="3">{exam.examData.CLASS}</td>
+              </tr>
+              <tr className="table-info">
+                <th colSpan="2">Teacher</th>
+                <td colSpan="3">{exam.examData.TEACHER}</td>
+              </tr>
+              <tr className="table-info">
+                <th colSpan="2">Time</th>
+                <td colSpan="3">{exam.examData.TIME}</td>
+              </tr>
+              <tr className="table-info">
+                <th colSpan="2">Total Marks</th>
+                <td colSpan="3">{exam.examData["TOTAL MARKS"]}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>S.No</th>
+                <th>Name</th>
+                <th>Register No.</th>
+                <th>Email</th>
+                <th>Marks</th>
+              </tr>
+              {studentDetails}
+            </tbody>
+          </table>
+        </>
+      );
+
+      count++;
+      parsedExams.push(item);
+    }
+
+    this.setState({ exams: parsedExams });
+  };
+
   componentDidMount() {
     this.parseHeader();
+    this.parseExams();
 
     console.log(this.props);
   }
@@ -75,6 +162,20 @@ class ReportPreview extends Component {
 
             <p>FILTERS ADDED :</p>
             <table>{this.state.header}</table>
+            <hr />
+            <br />
+
+            <p>RESULT : </p>
+            <table>
+              <tr>
+                <th width="150px">Total Exams</th>
+                <td>: {this.props.data.exams.length}</td>
+              </tr>
+            </table>
+            <hr />
+            <br />
+
+            {this.state.exams}
           </div>
         </div>
       </div>
